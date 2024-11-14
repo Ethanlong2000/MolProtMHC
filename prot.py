016fa455e113f77361cd2ff4f058df72a7d5130b
龙尧 <2571093134@qq.com>
2024-11-14 16:21:22 +0800

    first

---
+++ prot.py
@@ -0,0 +1,10 @@
+from transformers import BertModel, BertTokenizer
+import re
+tokenizer = BertTokenizer.from_pretrained("/home/longyh/software/prot_bert/", do_lower_case=False)
+model = BertModel.from_pretrained("/home/longyh/software/prot_b# ert/")
+sequence_Example = "A E T C Z A O"
+sequence_Example = "YYAMYREISTNTYESNLYLRYDSYTWAEWAYLWY"
+sequence_Example = re.sub(r"[UZOB]", "X", sequence_Example)
+encoded_input = tokenizer(sequence_Example, return_tensors='pt')
+output = model(**encoded_input)
+print(output)
