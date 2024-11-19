2adcadb79abdc72d28769752f9f0ad5e61e2a051
龙尧 <2571093134@qq.com>
2024-11-19 16:41:02 +0800

    新增prot.py、process_csv.py、ptest.py和mol.py文件，添加蛋白质分类器和CSV处理功能

---
+++ lightModule.py
@@ -0,0 +1,178 @@
+
+import torch
+from fast_transformers.attention import AttentionLayer
+from fast_transformers.events import QKVEvent
+from fast_transformers.transformers import TransformerEncoder, TransformerEncoderLayer
+from fast_transformers.builders.transformer_builders import BaseTransformerEncoderBuilder
+from fast_transformers.builders.attention_builders import AttentionBuilder
+
+from torch import nn
+from pubchem_encoder import Encoder
+import pytorch_lightning as pl
+from pytorch_lightning.utilities import seed
+
+from fast_transformers.feature_maps import GeneralizedRandomFeatures
+import torch.nn.functional as F
+from functools import partial
+
+from transformers import BertTokenizer
+import regex as re
+
+PATTERN = "(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
+class MolTranBertTokenizer(BertTokenizer):
+    def __init__(self, vocab_file: str = '',
+                 do_lower_case=False,
+                 unk_token='<pad>',
+                 sep_token='<eos>',
+                 pad_token='<pad>',
+                 cls_token='<bos>',
+                 mask_token='<mask>',
+                 **kwargs):
+        super().__init__(vocab_file,
+                         unk_token=unk_token,
+                         sep_token=sep_token,
+                         pad_token=pad_token,
+                         cls_token=cls_token,
+                         mask_token=mask_token,
+                         **kwargs)
+
+        self.regex_tokenizer = re.compile(PATTERN)
+        self.wordpiece_tokenizer = None
+        self.basic_tokenizer = None
+
+    def _tokenize(self, text):
+        split_tokens = self.regex_tokenizer.findall(text)
+        return split_tokens
+
+    def convert_tokens_to_string(self, tokens):
+        out_string = "".join(tokens).strip()
+        return out_string
+
+class RotaryEmbedding(torch.nn.Module):
+    
+    def __init__(self, dim, base=10000):
+        super().__init__()
+        inv_freq = 1. / (base ** (torch.arange(0, dim, 2).float() / dim))
+        self.register_buffer('inv_freq', inv_freq)
+        self.seq_len_cached = 0 
+        self.cos_cached = None
+        self.sin_cached = None
+
+    def forward(self, x, seq_dim=1):
+        seq_len = x.shape[seq_dim]
+        if seq_len != self.seq_len_cached:
+            self.seq_len_cached = seq_len
+            t = torch.arange(x.shape[seq_dim], device=x.device).type_as(self.inv_freq)
+            freqs = torch.einsum('i,j->ij', t, self.inv_freq)
+            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+            self.cos_cached = emb.cos()[None,:, None, :]
+            self.sin_cached = emb.sin()[None,:, None, :]
+                
+        return self.cos_cached, self.sin_cached
+
+def rotate_half(x):
+    x1, x2 = x[..., :x.shape[-1] // 2], x[..., x.shape[-1] // 2:]
+    return torch.cat((-x2, x1), dim=x1.ndim - 1)
+
+@torch.jit.script
+def apply_rotary_pos_emb(q, k, cos, sin):
+    return (q * cos) + (rotate_half(q) * sin), (k * cos) + (rotate_half(k) * sin)
+
+class RotateAttentionLayer(AttentionLayer):
+    def __init__(self, attention, d_model, n_heads, d_keys=None,
+                 d_values=None, event_dispatcher=""):
+        super(RotateAttentionLayer, self).__init__(attention,d_model, n_heads, d_keys=d_keys,
+                 d_values=d_values, event_dispatcher=event_dispatcher)
+
+        self.rotaryemb = RotaryEmbedding(d_keys)
+        print('Using Rotation Embedding')
+
+    def forward(self, queries, keys, values, attn_mask, query_lengths,
+                key_lengths):
+        N, L, _ = queries.shape
+        _, S, _ = keys.shape
+        H = self.n_heads
+
+        queries = self.query_projection(queries).view(N, L, H, -1)
+        keys = self.key_projection(keys).view(N, S, H, -1)
+        cos, sin = self.rotaryemb(queries)
+        queries, keys = apply_rotary_pos_emb(queries, keys, cos, sin)
+        values = self.value_projection(values).view(N, S, H, -1)
+        self.event_dispatcher.dispatch(QKVEvent(self, queries, keys, values))
+
+        new_values = self.inner_attention(
+            queries,
+            keys,
+            values,
+            attn_mask,
+            query_lengths,
+            key_lengths
+        ).view(N, L, -1)
+
+        return self.out_projection(new_values)
+
+class RotateEncoderBuilder(BaseTransformerEncoderBuilder):
+    def _get_attention_builder(self):
+        return AttentionBuilder()
+
+    def _get_attention_layer_class(self):
+        return RotateAttentionLayer
+
+    def _get_encoder_class(self):
+        return TransformerEncoder
+
+    def _get_encoder_layer_class(self):
+        return TransformerEncoderLayer
+    
+
+class LightningModule(pl.LightningModule):
+
+    def __init__(self, config, vocab):
+        super(LightningModule, self).__init__()
+
+        self.save_hyperparameters(config)
+        self.vocabulary = vocab
+        #location of cache File
+        # Special symbols
+
+        self.debug = config.debug
+        self.text_encoder = Encoder(config.max_len)
+        # Word embeddings layer
+        # n_vocab, d_emb = len(vocab.keys()), config.n_embd
+        n_vocab= len(vocab.keys())
+        # input embedding stem
+        builder = RotateEncoderBuilder.from_kwargs(
+            n_layers=config.n_layer,
+            n_heads=config.n_head,
+            query_dimensions=config.n_embd//config.n_head,
+            value_dimensions=config.n_embd//config.n_head,
+            feed_forward_dimensions=config.n_embd,
+            attention_type='linear',
+            #attention_type='full',
+            feature_map=partial(GeneralizedRandomFeatures, n_dims=config.num_feats),
+            activation='gelu',
+            )
+        self.pos_emb = None
+        self.tok_emb = nn.Embedding(n_vocab, config.n_embd)
+        self.drop = nn.Dropout(config.d_dropout)
+        ## transformer
+        self.blocks = builder.get()
+        self.lang_model = self.lm_layer(config.n_embd, n_vocab)
+        self.train_config = config
+        #if we are starting from scratch set seeds
+        if config.restart_path == "":
+            seed.seed_everything(config.seed)
+
+
+    class lm_layer(nn.Module):
+        def __init__(self, n_embd, n_vocab):
+            super().__init__()
+            self.embed = nn.Linear(n_embd, n_embd)
+            self.ln_f = nn.LayerNorm(n_embd)
+            self.head = nn.Linear(n_embd, n_vocab, bias=False)
+        def forward(self, tensor):
+            tensor = self.embed(tensor)
+            tensor = F.gelu(tensor)
+            tensor = self.ln_f(tensor)
+            tensor = self.head(tensor)
+            return tensor
